f81981a4201576369135ca48431581d0f1a81c8d
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-08 18:58:10 +0200

    [add] calcoli

--- es.dadi/js/script.js
+++ es.dadi/js/script.js
@@ -1,3 +1,17 @@
 // definizione variabili
 const pcNumber = Math.floor(Math.random() * 6 + 1);
 const userNumber = Math.floor(Math.random() * 6 + 1);
+
+//controllo quale numero è maggiore e definisco il vincitore
+if(pcNumber > userNumber){
+  // se il numero pc è maggiore a quello dell utente
+  console.log(`Numero pc: ${pcNumber}, numero utente: ${userNumber}. Ha vinto il pc`)
+}
+// se il numero del utente è maggiore a quello del pc
+else if(pcNumber < userNumber){
+  console.log(`Numero pc: ${pcNumber}, numero utente: ${userNumber}. Hai vinto`)
+}
+// se i due numeri sono uguali
+else if(pcNumber === userNumber){
+  console.log(`Numero pc: ${pcNumber}, numero utente: ${userNumber}. Pareggio`)
+}
