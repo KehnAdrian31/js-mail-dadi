7d1d6530988c428a16a3900c60275ade5ea09a73
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-08 17:43:49 +0200

    [add] mail list

---
+++ es.email/js/script.js
@@ -0,0 +1,4 @@
+//definizione delle variabili
+
+const emailList = [`carla88@gmail.com`, `jason12@yahoo.com`, `michele64@mail.com`, `rosalinda11@outlook.com`];
+const guessMail  = prompt(` Inserisci la tua email`)
