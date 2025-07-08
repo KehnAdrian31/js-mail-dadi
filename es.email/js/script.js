34bb82b1ca3daadecc1099bf344301d5d6706869
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-08 18:18:06 +0200

    [add] ciclo array

--- es.email/js/script.js
+++ es.email/js/script.js
@@ -2,3 +2,23 @@
 
 const emailList = [`carla88@gmail.com`, `jason12@yahoo.com`, `michele64@mail.com`, `rosalinda11@outlook.com`];
 const guessMail  = prompt(` Inserisci la tua email`)
+
+//variabile di controllo
+let check = false; 
+
+// ciclo array di email
+for( let i = 0; i < emailList.length; i++){
+  // controllo se la mail inserita è una dei invitati
+  if(guessMail === emailList[i]){
+    // se sì, allora metto la variabile di controllo a true
+    check = true;
+  }
+}
+// se la mail inserita è invitata
+if(check === true){
+  console.log(`auguri, lei è uno/a dei nostri invitati`)
+}
+// se la mail inserità non risulta nel sistema
+else{
+  console.log(`ci dispiace, lei non è invitato/a in festa`)
+}
